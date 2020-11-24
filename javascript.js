fe547613818b062027c91f241e98af5c8b795a2f
FrancescoAmato87 <francesco.duff@gmail.com>
2020-11-24 18:33:09 +0100

    Filter Part

--- javascript.js
+++ javascript.js
@@ -2,7 +2,8 @@
 const app = new Vue ({
     el: '#app',
     data: {
-        cds: []
+        cds: [],
+        actualGenre: 'all'
     },
     created(){
       // Get Albums List
@@ -15,6 +16,24 @@ const app = new Vue ({
            .catch(error => {
              console.log(error);
            });
-    }
+    },
 
+    methods:{
+      filterGenre () {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then(result => {
+          let cdsList = result.data.response;
+          // LA CONDIZIONE PER IL GENERE
+
+          if(this.ActualGenre !== 'all') {
+          cdsList = cdsList.filter( cd => cd.genre.toLowerCase() === this.actualGenre );
+          }
+
+          this.cds = cdsList;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      }
+    }
 });
